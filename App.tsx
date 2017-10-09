95767ba5d6af4cfcff610efae76c95505c6a5ff7
Jussi Laitinen <jussi.laitinen@digitalistgroup.com>
2017-10-09 12:03:57 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, ViewStyle, TextStyle } from 'react-native'
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.app}>
+        <Text style={styles.text}>Foo bar</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  app: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  } as ViewStyle,
+  text: {
+    color: 'black'
+  } as TextStyle
+})
